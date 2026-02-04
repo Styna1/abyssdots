27ea0bc4cbfb616b737c55b3996186512247d16d
Styna Starling <stynafox@gmail.com>
2026-02-04 12:43:22 -0600

    Push Dotfiles (Automated)

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,12 @@ source ~/.config/fish/custom/bindings.fish
 source ~/.config/fish/custom/functions.fish
 source ~/.config/fish/custom/prompt.fish
 
+
+export SCRIPTS="$HOME/scripts"
+export EDITOR=nvim
+export GIT_EDITOR=nvim
+
+
 function fish_greeting
     fastfetch
 end
@@ -22,3 +28,16 @@ if test -d ~/Applications/depot_tools
         set -p PATH ~/Applications/depot_tools
     end
 end
+
+if test -d "$HOME/scripts"
+    fish_add_path -aP "$HOME/scripts"
+end
+
+if test -d "$HOME/.local/bin"
+    fish_add_path -aP "$HOME/.local/bin"
+end
+
+if test -d "$HOME/.spicetify"
+    fish_add_path -aP "$HOME/.spicetify"
+end
+export PATH="$HOME/.local/bin:$PATH"
--- .config/fish/custom/prompt.fish
+++ .config/fish/custom/prompt.fish
@@ -2,19 +2,20 @@ function fish_prompt
     set -l git_info ""
     if command -sq git; and git rev-parse --is-inside-work-tree >/dev/null 2>&1
         set -l branch (git branch --show-current 2>/dev/null; or git describe --tags --exact-match 2>/dev/null; or git rev-parse --short HEAD)
-        
+
         set git_info " "(set_color blue)"$branch"(set_color normal)
-        
+
         if not git diff --cached --quiet 2>/dev/null
             set git_info $git_info" "(set_color green)"✚"(set_color normal)
         end
-        
+
         if not git diff --quiet 2>/dev/null
             set git_info $git_info" "(set_color red)"●"(set_color normal)
         end
     end
-    
+
     echo (prompt_pwd) $git_info
-    
-    echo -n (set_color brmagenta)"󰅂 "(set_color normal)
+
+    echo -n (set_color blue)""(set_color -b blue black)(whoami)(set_color normal)(set_color blue)" "(set_color brmagenta)"󰅂 "(set_color normal)
+
 end
